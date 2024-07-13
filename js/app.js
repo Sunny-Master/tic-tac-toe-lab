c6ee3a6f9642033c34b48a7a28145978e1497414
Sunny Master <master.codeworks@gmail.com>
2024-07-13 15:02:15 -0400

    Scores and Continue functionality added

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ let board
 let turn 
 let winner 
 let tie 
+let p1Score
+let p2Score
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
@@ -23,13 +25,30 @@ const messageEl = document.querySelector('#message')
 const boardEl = document.querySelector('.board')
 const resetBtnEl = document.querySelector('#reset')
 const contBtnEl = document.querySelector('#continue')
+const scoresEl = document.querySelector('#scores')
+
+const p1El = document.querySelector('#player1')
+const p2El = document.querySelector('#player2')
+
 
 /*-------------------------------- Functions --------------------------------*/
+
+// to toggle disable attributefor the 'Continue' button
+const enableContinue = () => {
+    if (contBtnEl.hasAttribute('disabled')) {
+        contBtnEl.removeAttribute('disabled')
+      }
+}
+
+const disableContinue = () => {
+    if (contBtnEl.hasAttribute('disabled') === false) {
+        contBtnEl.setAttribute('disabled', true)
+    }
+}
 
 const updateBoard = () => {
     board.forEach((box, idx) => {
         squareEls[idx].textContent = box
-        
     });
 }
 
@@ -40,12 +59,20 @@ const updateMessage = () => {
         messageEl.textContent = `It's a Tie!!`
     } else {
         messageEl.textContent = `Player ${turn} won! Congratulations!`
+
     }
+}
+
+//Show player Scores
+const showPlayerScores = () => {
+    p1El.textContent = `Player X: ${p1Score}`
+    p2El.textContent = `Player O: ${p2Score}`
 }
 
 const render = () => {
     updateBoard()
     updateMessage()
+    showPlayerScores()
 }
 
 const init = () => {
@@ -57,7 +84,10 @@ const init = () => {
     turn = 'X'
     winner = false
     tie = false
+    p1Score = 0
+    p2Score = 0   
     render()
+    disableContinue()
 }
 init()
 
@@ -75,6 +105,12 @@ const checkForWinner = () => {
         })
         if (winCombo[0] !== '' && winCombo[0] === winCombo[1] && winCombo[0] === winCombo[2]) {
             winner = true
+            enableContinue()
+            if (turn === 'X') {
+                p1Score++
+            } else {
+                p2Score++
+            }
         }
     })
 }
@@ -94,6 +130,7 @@ const checkForTie = () => {
         return
     } 
     tie = true
+    enableContinue()
 }
 
 // switch Player Turn
@@ -121,11 +158,28 @@ const handleClick = (event) => {
     render()
 }
 
+// cleaning the board to continue playing
+const contInit = () => {
+    board = [
+        '', '', '',
+        '', '', '', 
+        '', '', '',
+    ]
+    winner = false
+    tie = false
+    switchPlayerTurn()
+    render()
+    disableContinue()
+}
 
-
+//Continue the game with scoring
+const continueGame = () => {
+    contInit()
+    
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 boardEl.addEventListener('click', handleClick)
 resetBtnEl.addEventListener('click', init)
-
+contBtnEl.addEventListener('click', continueGame)
