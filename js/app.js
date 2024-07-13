2cb57b41214fc82d101dc984051b0c834feee7c2
Sunny Master <master.codeworks@gmail.com>
2024-07-13 11:08:20 -0400

    Game Play Completed (Reset button will be added in the next update)

---
+++ js/app.js
@@ -0,0 +1,131 @@
+/*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+]
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board 
+let turn 
+let winner 
+let tie 
+
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
+const boardEl = document.querySelector('.board')
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    console.log('game initiated')
+    board = [
+        '', '', '',
+        '', '', '', 
+        '', '', '',
+       ]
+    turn = 'X'
+    winner = false
+    tie = false
+}
+init()
+
+const updateBoard = () => {
+    board.forEach((box, idx) => {
+        squareEls[idx].textContent = box
+        
+    });
+}
+
+const updateMessage = () => {
+    if (winner === false && tie === false) {
+        messageEl.textContent = `It's the turn of Player ${turn}. Please click on an empty square!`
+    } else if (winner === false && tie) {
+        messageEl.textContent = `It's a Tie!!`
+    } else {
+        messageEl.textContent = `Player ${turn} won! Congratulations!`
+    }
+}
+
+const render = () => {
+    updateBoard()
+    updateMessage()
+}
+
+render()
+
+//place Piece on the board
+const placePiece = (index) => {
+    board[index] = turn
+    console.log(board)
+}
+
+//check for winner
+const checkForWinner = () => {
+    winningCombos.forEach(winningCombo => {
+        let winCombo = []
+        winningCombo.forEach(pos => {
+            winCombo.push(board[pos])
+        })
+        if (winCombo[0] !== '' && winCombo[0] === winCombo[1] && winCombo[0] === winCombo[2]) {
+            winner = true
+        }
+    })
+}
+
+//check for Tie
+const checkForTie = () => {
+    if (winner) { 
+        return
+    }
+    let emptyCount = 0
+    board.forEach(sqr => {
+        if (sqr === '') {
+            emptyCount++
+        }
+    })
+    if (emptyCount) {
+        return
+    } 
+    tie = true
+}
+
+// switch Player Turn
+const switchPlayerTurn = () => {
+    if (winner) {
+        return
+    } else if (turn === 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
+    console.log(turn)
+}
+
+const handleClick = (event) => {
+    const squareIndex = event.target.id
+    if (board[squareIndex]) {
+        return
+    } else if (winner) {
+        return
+    }
+    placePiece(squareIndex)
+    checkForWinner()
+    checkForTie()
+    switchPlayerTurn()
+    render()
+}
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+boardEl.addEventListener('click', handleClick)
+
